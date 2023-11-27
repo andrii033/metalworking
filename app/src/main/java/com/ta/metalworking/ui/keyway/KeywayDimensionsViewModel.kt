91123dc1c76eb93c87a29bf68c9caeb1fde30c48
andrii <ataran033@gmail.com>
2023-11-27 20:35:51 +0100

    add key way to nav menu

--- app/src/main/java/com/ta/metalworking/ui/keyway/KeywayDimensionsViewModel.kt
+++ app/src/main/java/com/ta/metalworking/ui/keyway/KeywayDimensionsViewModel.kt
@@ -1,7 +1,12 @@
 package com.ta.metalworking.ui.keyway
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class KeywayDimensionsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is kay way Fragment"
+    }
+    val text: LiveData<String> = _text
 }
